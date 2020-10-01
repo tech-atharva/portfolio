40577cb80e75b9554d66e35b34b035532e1e07cd
tech-atharva <gatly0145@gmail.com>
2020-10-01 17:29:45 +0530

    boom

---
+++ javascript/app.js
@@ -0,0 +1,134 @@
+const hidden = (element) => {
+    const elements = document.querySelector(`${element}`)
+    elements.style.display = 'none';
+}
+const show = (element) => {
+    const elements = document.querySelector(`${element}`)
+    elements.style.display = 'flex';
+}
+hidden('.body')
+const load = () => {
+    hidden('.mid-loader')
+    show('.body')
+}
+
+const toggle = () => {
+    const elem = document.querySelector('.burger')
+    const nav = document.querySelector('.nav');
+    nav.style.height = 'auto';
+    elem.addEventListener('click', () => {
+        nav.style.height = '0px';
+        elem.addEventListener('click', () => {
+            nav.style.height = 'auto';
+            elem.addEventListener('click', () => {
+                nav.style.height = '0px';
+                elem.addEventListener('click', () => {
+                    nav.style.height = 'auto';;
+                    elem.addEventListener('click', () => {
+                        nav.style.height = 'auto';
+                        elem.addEventListener('click', () => {
+                            nav.style.height = '0px';
+                            elem.addEventListener('click', () => {
+                                nav.style.height = 'auto';
+                                elem.addEventListener('click', () => {
+                                    nav.style.height = '0px';
+                                    elem.addEventListener('click', () => {
+                                        nav.style.height = 'auto';
+                                        elem.addEventListener('click', () => {
+                                            nav.style.height = '0px';
+                                            elem.addEventListener('click', () => {
+                                                nav.style.height = 'auto';
+                                                elem.addEventListener('click', () => {
+                                                    nav.style.height = '0px';
+                                                    elem.addEventListener('click', () => {
+                                                        nav.style.height = 'auto';
+                                                        elem.addEventListener('click', () => {
+                                                            nav.style.height = '0px';
+                                                            elem.addEventListener('click', () => {
+                                                                nav.style.height = 'auto';
+                                                                elem.addEventListener('click', () => {
+                                                                    nav.style.height = '0px';
+                                                                    elem.addEventListener('click', () => {
+                                                                        nav.style.height = 'auto';
+                                                                        elem.addEventListener('click', () => {
+                                                                            nav.style.height = '0px';
+                                                                            elem.addEventListener('click', () => {
+                                                                                nav.style.height = 'auto';
+                                                                                elem.addEventListener('click', () => {
+                                                                                    nav.style.height = '0px';
+                                                                                    elem.addEventListener('click', () => {
+                                                                                        nav.style.height = 'auto';
+                                                                                        elem.addEventListener('click', () => {
+                                                                                            nav.style.height = '0px';
+                                                                                            elem.addEventListener('click', () => {
+                                                                                                nav.style.height = 'auto';
+                                                                                                elem.addEventListener('click', () => {
+                                                                                                    nav.style.height = '0px';
+                                                                                                    elem.addEventListener('click', () => {
+                                                                                                        nav.style.height = 'auto';
+                                                                                                        elem.addEventListener('click', () => {
+                                                                                                            nav.style.height = '0px';
+                                                                                                            elem.addEventListener('click', () => {
+                                                                                                                nav.style.height = 'auto';
+                                                                                                                elem.addEventListener('click', () => {
+                                                                                                                    nav.style.height = '0px';
+                                                                                                                    elem.addEventListener('click', () => {
+                                                                                                                        nav.style.height = 'auto';
+                                                                                                                        elem.addEventListener('click', () => {
+                                                                                                                            nav.style.height = '0px';
+                                                                                                                            elem.addEventListener('click', () => {
+                                                                                                                                nav.style.height = 'auto';
+                                                                                                                                elem.addEventListener('click', () => {
+                                                                                                                                    nav.style.height = '0px';
+                                                                                                                                    elem.addEventListener('click', () => {
+                                                                                                                                        nav.style.height = 'auto';
+                                                                                                                                        elem.addEventListener('click', () => {
+                                                                                                                                            nav.style.height = '0px';
+                                                                                                                                            elem.addEventListener('click', () => {
+                                                                                                                                                nav.style.height = 'auto';
+                                                                                                                                                elem.addEventListener('click', () => {
+                                                                                                                                                    nav.style.height = '0px';
+                                                                                                                                                    elem.addEventListener('click', () => {
+                                                                                                                                                        nav.style.height = 'auto';
+                                                                                                                                                        elem.addEventListener('click', () => {
+                                                                                                                                                            nav.style.height = '0px';
+                                                                                                                                                        })
+                                                                                                                                                    })
+                                                                                                                                                })
+                                                                                                                                            })
+                                                                                                                                        })
+                                                                                                                                    })
+                                                                                                                                })
+                                                                                                                            })
+                                                                                                                        })
+                                                                                                                    })
+                                                                                                                })
+                                                                                                            })
+                                                                                                        })
+                                                                                                    })
+                                                                                                })
+                                                                                            })
+                                                                                        })
+                                                                                    })
+                                                                                })
+                                                                            })
+                                                                        })
+                                                                    })
+                                                                })
+                                                            })
+                                                        })
+                                                    })
+                                                })
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Atharva portfolio website
+
+### The Passion of WebDev
+
+## About Me
+
+Hi!, I'am 'Atharva' i'm a web designer.
+
+### @atharva-xyx @atharvaavhad @tech-atharva
+
+@codewithary
+@tapatechnical
+@shivamjoker
+
+# some things I have
+
+- technical
+- progrmmer
+- progrmmer
+- @react
+- cray boy
+
+This What Can I do?
+
+I am Atharva and all they are @atharva-xyx @atharvaavhad @tech-atharva they are my freinds yo.
